09d54c9689c50bce6397a87771cfe6e257954577
Elisa Canelo <elisacaneloc@gmail.com>
2018-03-20 17:18:07 -0300

    se agrega descripción de archivo y funcion suma

--- prueba.py
+++ prueba.py
@@ -1,1 +1,9 @@
 #! /usr/bin/env python
+
+# Ahora cambiamos el archivo para hacer una nueva rama de prueba para el
+# desafio
+# Agregaremos una función cualquiera
+
+
+def suma(a, b):
+    return a + b
